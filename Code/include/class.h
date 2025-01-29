7167983e1caebc12a8a53234b9f2a190ef0f5b97
ChristopherBennett <chrisjwbennett@gmail.com>
2025-01-29 20:09:32 +0000

    Adding stationary aliens and adding player that can move horizontally

--- Code/include/class.h
+++ Code/include/class.h
@@ -11,6 +11,53 @@
 #include <memory>
 #include <vector>
 
+
+class GameObj
+{
+    public:
+        GameObj(double newX=-1, double newY=-1, int newWidth=20, int newHeight=20, double newDx=0.5, double newDy=2, Color newColor=BLACK);
+
+        void changeX(int newX);
+        void changeY(int newY);
+        void changeDx(int newDx);
+        void changeDy(int newDy);
+
+        int getX();
+        int getY();
+        int getWidth();
+        int getHeight();
+        int getDx();
+        int getDy();
+        Color getColor();
+
+        virtual void draw();
+        virtual void update();
+
+    protected:
+        int 
+            width,
+            height;
+        double
+            x,
+            y,
+            dx,
+            dy;
+        Color color;
+};
+
+class Player: public GameObj
+{
+    public:
+        Player() : GameObj(GetScreenWidth()/2, GetScreenHeight()*0.8, 20, 20, 0.5, 2, BLUE) {}
+
+        void draw() override;
+        void update() override;
+};
+
+class Bullet : public GameObj
+{
+};
+
 class Window
 {
     public:
@@ -29,25 +76,13 @@ class Window
         Color color;
 };
 
-class GameObj
-{
-    public:
-        GameObj(int newX, int newY, int newWidth, int newHeight, Color newColor=BLACK);
-
-    protected:
-        int 
-            width,
-            height,
-            x,
-            y;
-        Color color;
-};
-
 class Game 
 {
     public:
         Game();
-        //Make window 
+        bool makeGameObjs(int num=30);
+        void drawGameObjs();
+        void updateGameObjs();
         void run();
 
     private:
@@ -55,6 +90,5 @@ class Game
         std::unique_ptr<Window> window = std::make_unique<Window>();
 };
 
-#endif
 
-        
+#endif
--- Code/src/class.cpp
+++ Code/src/class.cpp
@@ -8,25 +8,66 @@
 #include <iostream>
 #include "raylib.h"
 
-//Game
+#define LINEONE 50
+#define LINETWO 100
+#define LINETHREE 150
+
+//------------------Game--------------------
 Game::Game()
 {
     window = std::make_unique<Window>();
+}
+
+bool Game::makeGameObjs(int num)
+{
+    try
+    {
+        objs.push_back(std::make_unique<Player>());
+        for (int i=1; i < num ; i++)
+        {
+            objs.push_back(std::make_unique<GameObj>());
+            int objX = i*(objs[i]->getWidth()+20) + 50;
+            objs[i]->changeX(objX);
+            objs[i]->changeY(LINEONE);
+        }
+    }catch (std::exception& e) { return false;}
+    return true;
+}
+
+void Game::drawGameObjs()
+{
+    for (int j=0; j < objs.size() ; j++)
+    {
+        objs[j]->draw();
+    }
+}
+
+void Game::updateGameObjs()
+{
+    for (auto& obj : objs)
+    {
+        obj->update();
+    }
+
 }
 
 void Game::run()
 {
     const char* gameTitle = window->getTitle();
     InitWindow(window->getWidth(), window->getHeight(), gameTitle);
+    makeGameObjs(30);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
             ClearBackground(window->getColor());
+            updateGameObjs();
+            drawGameObjs();
         EndDrawing();
     }
 }
-//Window
+
+//----------Window----------------
 Window::Window(int newWidth, int newHeight, Color newColor)
 {
     width = newWidth;
@@ -36,12 +77,73 @@ Window::Window(int newWidth, int newHeight, Color newColor)
 }
 
 
-//GameObj
-GameObj::GameObj(int newX, int newY, int newWidth, int newHeight, Color newColor)
+//--------------GameObj-------------------------
+GameObj::GameObj(double newX, double newY, int newWidth, int newHeight, double newDx, double newDy, Color newColor)
 {
     x = newX;
     y = newY;
     width = newWidth;
     height = newHeight;
     color = newColor;
+    dx = newDx;
+    dy = newDy;
+}
+
+void GameObj::changeX(int newX) { x = newX;}
+void GameObj::changeY(int newY) { y = newY;}
+int GameObj::getWidth() { return width;}
+int GameObj::getHeight() {return height;}
+int GameObj::getX() { return x;}
+int GameObj::getY() { return y;}
+
+void GameObj::update(){}
+
+void GameObj::draw()
+{
+    std::pair<int, int> innerStrip = {x - width/2 + width/3, x - width/2 + width*0.6};
+    for (int i=x - width/2; i < (x + width/2) ; i++)
+    {
+        for (int j=y - height/2; j < (y+height/2) ; j++)
+        {
+            if (i < innerStrip.first || i > innerStrip.second){
+                DrawPixel(i, j, WHITE);
+            }
+            else {
+                DrawPixel(i, j, color);
+            }
+        }
+    }
+}
+
+//---------------Player-------------------------------
+void Player::update()
+{
+    if (IsKeyDown(KEY_LEFT)){
+        if (x - dx >= width/2 + 5){
+            x -= dx;
+            std::cout << "Moving Left" << std::endl;
+        }
+    }
+    else if (IsKeyDown(KEY_RIGHT)){
+        if (x + dx <= GetScreenWidth() - (width/2 + 5)){
+            x += dx;
+            std::cout << "Moving Right" << std::endl;
+        }
+    }
+}
+
+void Player::draw()
+{
+    for (int i=x - width/2; i < x + width/2 ; i++)
+    {
+        for (int j=y - height/2; j < y + height/2 ; j++)
+        {
+            if ((i - x)*(i - x) + (j - y)*(j-y) <= (width/2)*(width/2)){
+                if ((i - x) * (i - x) + (j - y)*(j-y) >= (width/4)*(width/4)){
+                    DrawPixel(i, j, color);
+                }
+            }
+        }
+    }
+    DrawCircleLines(x, y, width/2, RAYWHITE);
 }
